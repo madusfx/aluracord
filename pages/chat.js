940242a6048f88fc2156a009cbe52630780d45fa
Maria Eduarda <mariaesfreitas@gmail.com>
2022-01-27 19:41:28 -0300

    aula 04

--- pages/chat.js
+++ pages/chat.js
@@ -1,23 +1,59 @@
 import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzMwOTM2MCwiZXhwIjoxOTU4ODg1MzYwfQ.K86iUMZzywgO7LpsFiUKksvjwjpziGRm8cTD7bd2qnM';
+const SUPABASE_URL = 'https://bdsfimngsrbulrrglvgn.supabase.co';
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default function ChatPage() {
     const [message, setMessage] = React.useState('');
     const [messageList, setMessageList] = React.useState([]);
 
+    React.useEffect(() => {
+      supabaseClient
+        .from('messages')
+        .select('*')
+        .order('id', { ascending: false })
+        .then(({ data }) => {
+          console.log('Dados da consulta:', data);
+          setMessageList(data);
+        })
+    }, []);
+
     function handleNewMessage(newMessage) {
         const message = {
-            id: messageList.length + 1,
             from: 'madusfx',
             text: newMessage,
         };
 
-        setMessageList([
-            message,
-            ...messageList,
-        ]);
-        setMessage('');
+        supabaseClient
+          .from('messages')
+          .insert([
+            message
+          ])
+          .then((data) => {
+            console.log('criando mensahgem', data);
+            setMessageList([
+              data.data[0],
+              ...messageList,
+          ]);
+          })
+          setMessage('');
+    }
+
+    function deleteMessage(currentMessage) {
+      supabaseClient
+        .from('messages')
+        .delete()
+        .match({id: currentMessage.id})
+        .then(({data}) => {
+          const messagesListFiltered = messageList.filter((message) => {
+            return message.id != data[0].id
+          });
+          setMessageList(messagesListFiltered);
+        });
     }
 
   return (
@@ -57,7 +93,7 @@ export default function ChatPage() {
             padding: '16px',
           }}
         >
-          <MessageList mensagens={messageList} />
+          <MessageList mensagens={messageList} deleteMessage={deleteMessage}/>
           {/* {listaDeMensagens.map((mensagemAtual) => {
               return (
                   <li key={mensagemAtual.id}>
@@ -72,6 +108,7 @@ export default function ChatPage() {
               alignItems: 'center',
             }}
           >
+            
             <TextField
               value={message}
               onChange={(event) => {
@@ -112,17 +149,21 @@ function Header() {
           Chat
         </Text>
         <Button
-            variant='tertiary'
-            colorVariant='neutral'
-            label='Logout'
-            href="/"
+          variant='tertiary'
+          colorVariant='neutral'
+          label='Logout'
+          href="/"
         />
       </Box>
     </>
   )
 }
 
+
+
 function MessageList(props) {
+  const deleteMessage = props.deleteMessage;
+
   return (
     <Box
       tag="ul"
@@ -133,8 +174,10 @@ function MessageList(props) {
         flex: 1,
         color: appConfig.theme.colors.primary['100'],
         marginBottom: '16px',
+        overflow: 'auto',
       }}
     >
+      
       {props.mensagens.map((message) => {
         return (
           <Text
@@ -162,7 +205,7 @@ function MessageList(props) {
                   display: 'inline-block',
                   marginRight: '8px',
                 }}
-                src={`https://github.com/madusfx.png`}
+                src={`https://github.com/${message.from}.png`}
               />
               <Text tag="strong">
                 {message.from}
@@ -177,6 +220,23 @@ function MessageList(props) {
               >
                 {(new Date().toLocaleDateString())}
               </Text>
+              <Button 
+                styleSheet={{
+                  borderRadius: '25%',
+                  width: '12px',
+                  marginLeft: '8px'
+                }}
+                variant='tertiary'
+                colorVariant='dark'
+                label={<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>}
+                buttonColors={{
+                  mainColor: appConfig.theme.colors.primary['100'],
+                }}
+                onClick={(event) => {
+                  event.preventDefault()
+                  deleteMessage(message)
+                }}
+              />
             </Box>
             {message.text}
           </Text>
